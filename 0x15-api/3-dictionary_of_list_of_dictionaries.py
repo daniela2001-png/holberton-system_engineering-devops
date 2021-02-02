36819e05a3f705d019b4fb3b63ad053af7a19633
daniela2001-png <dmbmora@outlook.com>
2021-02-01 19:55:13 -0500

    adding point three

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+using the module requests to get data
+of the API
+"""
+from sys import argv
+import requests
+import json
+
+if __name__ == "__main__":
+
+    response_todo = requests.get(
+        "https://jsonplaceholder.typicode.com/todos")
+    todos = response_todo.json()
+
+    names_users = requests.get(
+        "https://jsonplaceholder.typicode.com/users")
+    users = names_users.json()
+    json_obj = {}
+    for user in users:
+        username = user["username"]
+        my_list = []
+        for task in todos:
+            if (user["id"] == task["userId"]):
+                my_dict = {}
+                my_dict["task"] = task['title']
+                my_dict["completed"] = task['completed']
+                my_dict["username"] = username
+                my_list.append(my_dict)
+        json_obj[user["id"]] = my_list
+    with open("todo_all_employees.json", 'w') as jsonfile:
+        json.dump(json_obj, jsonfile)
